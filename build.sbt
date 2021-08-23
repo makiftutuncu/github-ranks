51fabd235a945170d9dd14f98028eeb643bddd6e
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2021-08-23 23:48:32 +0300

    Initialize an http4s app

--- build.sbt
+++ build.sbt
@@ -1,4 +1,32 @@
-name             := "github-ranks"
-version          := "0.1"
-scalaVersion     := "3.0.1"
-idePackagePrefix := Some("dev.akif.githubranks")
+name         := "github-ranks"
+version      := "0.1"
+scalaVersion := "3.0.1"
+
+val catsEffectVersion      = "3.2.3"
+val circeVersion           = "0.14.1"
+val http4sVersion          = "1.0.0-M24"
+val logbackVersion         = "1.2.5"
+val munitCatsEffectVersion = "1.0.5"
+val munitVersion           = "0.7.28"
+val scalaLoggingVersion    = "3.9.4"
+
+libraryDependencies ++= Seq(
+  "ch.qos.logback"              % "logback-classic"     % logbackVersion,
+  "io.circe"                   %% "circe-core"          % circeVersion,
+  "io.circe"                   %% "circe-generic"       % circeVersion,
+  "io.circe"                   %% "circe-parser"        % circeVersion,
+  "org.http4s"                 %% "http4s-circe"        % http4sVersion,
+  "org.http4s"                 %% "http4s-dsl"          % http4sVersion,
+  "org.http4s"                 %% "http4s-ember-client" % http4sVersion,
+  "org.http4s"                 %% "http4s-ember-server" % http4sVersion,
+  "org.typelevel"              %% "cats-effect"         % catsEffectVersion,
+  "com.typesafe.scala-logging" %% "scala-logging"       % scalaLoggingVersion,
+
+  "org.scalameta" %% "munit"               % munitVersion           % Test,
+  "org.typelevel" %% "munit-cats-effect-3" % munitCatsEffectVersion % Test
+
+)
+
+testFrameworks += new TestFramework("munit.Framework")
+
+idePackagePrefix.withRank(KeyRanks.Invisible) := Some("dev.akif.githubranks")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,43 @@
 package dev.akif.githubranks
 
-object Main:
-  def main(args: Array[String]): Unit =
-    println("Hello world!")
+import cats.effect.*
+import com.comcast.ip4s.{Host, Port}
+import com.typesafe.scalalogging.LazyLogging
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.*
+import org.http4s.dsl.io.*
+import org.http4s.implicits.*
+import org.http4s.server.Router
+
+import scala.util.control.NonFatal
+
+object Main extends IOApp with LazyLogging:
+  lazy val helloRoute: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case GET -> Root =>
+      Ok("Hello world!")
+  }
+
+  lazy val httpApp: HttpApp[IO] = Router("/" -> helloRoute).orNotFound
+
+  lazy val errorHandler: PartialFunction[Throwable, IO[Response[IO]]] =
+    case NonFatal(e) =>
+      val response = Response[IO]()
+        .withEntity(e.getMessage)
+        .withStatus(Status.InternalServerError)
+
+      IO.delay(logger.error("Unhandled error", e)) *> IO.pure(response)
+
+  override def run(args: List[String]): IO[ExitCode] =
+    for
+      host     <- IO.fromOption(Host.fromString("localhost"))(new IllegalArgumentException("Invalid host!"))
+      port     <- IO.fromOption(Port.fromInt(8080))(new IllegalArgumentException("Invalid port!"))
+      exitCode <- EmberServerBuilder.default[IO]
+                                    .withHost(host)
+                                    .withPort(port)
+                                    .withHttpApp(httpApp)
+                                    .withErrorHandler(errorHandler)
+                                    .build
+                                    .use(_ => IO.never)
+                                    .as(ExitCode.Success)
+    yield
+      exitCode
